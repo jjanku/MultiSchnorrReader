2a037003fa897b990e5f7f3e4c16b729c550844b
Jakub Janků <janku.jakub.jj@gmail.com>
2023-03-07 21:21:56 +0100

    implement PossessedKey

--- app/src/main/java/com/example/multischnorrreader/crypto/PossessedKey.kt
+++ app/src/main/java/com/example/multischnorrreader/crypto/PossessedKey.kt
@@ -1,5 +1,33 @@
 package com.example.multischnorrreader.crypto
 
+import org.bouncycastle.crypto.digests.SHA256Digest
+import org.bouncycastle.crypto.params.ECPrivateKeyParameters
+import org.bouncycastle.crypto.params.ECPublicKeyParameters
+import org.bouncycastle.crypto.signers.DSADigestSigner
+import org.bouncycastle.crypto.signers.ECDSASigner
 import org.bouncycastle.math.ec.ECPoint
+import java.math.BigInteger
 
-data class PossessedKey(val key: ECPoint, val pop: ByteArray)
+class PossessedKey(val key: ECPoint, val pop: ByteArray) {
+    fun verify(): Boolean = createSigner().run {
+        init(false, ECPublicKeyParameters(key, SchemeParameters.ec))
+        val data = key.getEncoded(false)
+        update(data, 0, data.size)
+        verifySignature(pop)
+    }
+
+    companion object {
+        private fun createSigner() = DSADigestSigner(ECDSASigner(), SHA256Digest())
+
+        fun fromPrivate(private: BigInteger): PossessedKey {
+            val key = private * SchemeParameters.ec.g
+            val pop = createSigner().run {
+                init(true, ECPrivateKeyParameters(private, SchemeParameters.ec))
+                val data = key.getEncoded(false)
+                update(data, 0, data.size)
+                generateSignature()
+            }
+            return PossessedKey(key, pop)
+        }
+    }
+}
