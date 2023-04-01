e87f440b8c2b3208b5f7e86ff6824cf0be8d2b88
Jakub Janků <janku.jakub.jj@gmail.com>
2023-04-01 13:46:20 +0200

    test: configure simulator
    Allow prime RSA modulus to enable jcmathlib point multiplication
    using x-coordinate DH.
    
    Use SecureRandom instead of the default fixed seed for RandomData.

--- app/src/test/java/com/example/multischnorrreader/SimulatedAppletCosigner.kt
+++ app/src/test/java/com/example/multischnorrreader/SimulatedAppletCosigner.kt
@@ -12,6 +12,11 @@ class SimulatedAppletCosigner : AppletCosigner() {
         simulator.transmitCommand(data)
 
     override suspend fun init() {
+        // Otherwise requires modulus to be composite
+        System.setProperty("com.licel.jcardsim.bouncycastle.rsa.allow_unsafe_mod", "true");
+        // Set to seed RandomData from the SecureRandom
+        System.setProperty("com.licel.jcardsim.randomdata.secure", "1");
+
         val aid = AIDUtil.create(AID)
         val jar = File(APPLET_JAR_PATH).readBytes()
         with(simulator) {
