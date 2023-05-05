962b09a11e3a975a920ac2ee704c90bd6fc48aa4
Jakub Janků <janku.jakub.jj@gmail.com>
2023-05-05 21:12:42 +0200

    improve serialization
    fix conversion of the message bytearray,
    use decimal representation for easier json decoding

--- app/src/main/java/com/example/multischnorrreader/util/Serialization.kt
+++ app/src/main/java/com/example/multischnorrreader/util/Serialization.kt
@@ -4,33 +4,32 @@ import com.example.multischnorrreader.SignState
 import org.bouncycastle.math.ec.ECPoint
 import java.math.BigInteger
 
-fun BigInteger.toHex(): String = "0x${toString(16)}"
+fun ECPoint.coords(): Pair<BigInteger, BigInteger> = normalize().run {
+    Pair(xCoord.toBigInteger(), yCoord.toBigInteger())
+}
 
-fun ECPoint.toHex(): Pair<String, String> = normalize().run {
-    Pair(
-        xCoord.toBigInteger().toHex(),
-        yCoord.toBigInteger().toHex()
-    )
+fun ByteArray.toHex() = joinToString(separator = "") {
+    it.toUByte().toString(16).padStart(2, '0')
 }
 
 fun SignState.Success.toJson(): String {
-    val (Xx, Xy) = group.toHex()
-    val m = message.joinToString(separator = "") { it.toUByte().toString(16) }
-    val (Rx, Ry) = signature.nonce.toHex()
-    val s = signature.value.toHex()
+    val (Xx, Xy) = group.coords()
+    val m = message.toHex()
+    val (Rx, Ry) = signature.nonce.coords()
+    val s = signature.value
 
     return """
         {
             "group": {
-                "x": "$Xx",
-                "y": "$Xy"
+                "x": $Xx,
+                "y": $Xy
             },
             "message": "$m",
             "nonce": {
-                "x": "$Rx",
-                "y": "$Ry"
+                "x": $Rx,
+                "y": $Ry
             },
-            "signature": "$s"
+            "signature": $s
         }
     """.trimIndent()
 }
